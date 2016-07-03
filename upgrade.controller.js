f07b0b62f719ebb8c28f8c0e802d138ae18bf847
St-Ex <stexthomas@gmail.com>
2016-07-03 20:15:06 +0200

    upgrade find spawns FIX

--- upgrade.controller.js
+++ upgrade.controller.js
@@ -25,7 +25,7 @@ module.exports = class UpgradeController extends Controller {
             }
           }
           else {
-            var sources = creep.room.find(FIND_SPAWNS);
+            var sources = creep.room.find(FIND_MY_SPAWNS);
             if (creep.harvest(sources[ 0 ]) == ERR_NOT_IN_RANGE) {
               creep.moveTo(sources[ 0 ]);
             }

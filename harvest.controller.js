c47139a0f0442975374642080d9b8a93525ad420
St-Ex <stexthomas@gmail.com>
2016-07-03 19:17:07 +0200

    harvest

--- harvest.controller.js
+++ harvest.controller.js
@@ -8,9 +8,39 @@ module.exports = class HarvestController extends Controller {
 
   control () {
     super.control();
+    this.sources = creep.room.find(FIND_SOURCES);
+
+    if (this.creeps.length > 0) {
+      this.creeps.forEach(
+        creep => {
+          if (creep.carry.energy < creep.carryCapacity) {
+            this.harvestOrMove(creep)
+          }
+          else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+              filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                  structure.energy < structure.energyCapacity;
+              }
+            });
+            if (targets.length > 0) {
+              if (creep.transfer(targets[ 0 ], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[ 0 ]);
+              }
+            }
+          }
+        }
+      )
+    }
+  }
+
+  harvestOrMove (creep) {
+    if (creep.harvest(this.sources[ 0 ]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(this.sources[ 0 ]);
+    }
   }
 
   newCreep () {
-    return [ WORK, MOVE, CARRY ]
+    return [ WORK, WORK, MOVE, CARRY ]
   }
 }

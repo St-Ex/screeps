b29568c0163f8288f4124e336a410504b5a1b4ae
Thomas de Saint-Exupéry <thomas.desaintexupery@bappli.com>
2016-07-04 09:23:20 +0200

    Fix memory clean up

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const BUILD_CONT = new BuildController(3);
 
 module.exports.loop = function () {
 
-  for (var name in Memory.creep) {
+  for (var name in Memory.creeps) {
     if (!Game.creeps[ name ]) {
       delete Memory.creeps[ name ]
     }

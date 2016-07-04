3cac87ccbb4810bcf301d3962084cf66f80e7c3f
St-Ex <stexthomas@gmail.com>
2016-07-04 20:08:34 +0200

    build what can be built

--- creep.manager.js
+++ creep.manager.js
@@ -1,57 +1,74 @@
 const trigger_energy = 0.60
 
+const PART_COST = {
+  MOVE: 50,
+  WORK: 100,
+  CARRY: 50,
+  ATTACK: 80,
+  RANGED_ATTACK: 150,
+  HEAL: 250,
+  CLAIM: 600,
+  TOUGH: 10
+}
+
 class CreepManager {
 
-	constructor() {
-		this.creeps = {}
-		this.en_creeps = []
-	}
+  constructor () {
+    this.creeps = {}
+    this.en_creeps = []
+  }
 
-	reset(init) {
+  reset (init) {
 
-		this.creeps = Object.assign({}, init)
-		this.en_creeps = []
+    this.creeps = Object.assign({}, init)
+    this.en_creeps = []
 
-		for (let name in Game.creeps) {
-			let creep = Game.creeps[name]
+    for (let name in Game.creeps) {
+      let creep = Game.creeps[ name ]
 
-			if (creep) {
-				if (!creep.memory.role) {
-					// Recycle
-					creep.memory.role = 'recycle';
-				}
-				this.creeps[creep.memory.role].add(creep.id)
+      if (creep) {
+        if (!creep.memory.role) {
+          // Recycle
+          creep.memory.role = 'recycle';
+        }
+        this.creeps[ creep.memory.role ].add(creep.id)
 
-				if (creep.carry.RESOURCE_ENERGY) {
-					let p = creep.carry.RESOURCE_ENERGY / creep.carryCapacity
-					if (p < trigger_energy) {
-						this.en_creeps.push[{id: creep.id, p: p}];
-					}
-				}
-			}
-		}
+        if (creep.carry.RESOURCE_ENERGY) {
+          let p = creep.carry.RESOURCE_ENERGY / creep.carryCapacity
+          if (p < trigger_energy) {
+            this.en_creeps.push[ { id: creep.id, p: p } ];
+          }
+        }
+      }
+    }
 
-		this.en_creeps.sort((c1,c2)=>c2.p-c1.p)
-	}
+    this.en_creeps.sort((c1, c2)=>c2.p - c1.p)
+  }
 
-	spawn(spawner, parts, memory) {
-		if (!spawner.spawning && spawner.canCreateCreep(parts) === 0) {
-			spawner.createCreep(parts, null, memory)
-		}
-	}
+  spawn (spawner, parts, memory) {
+    let max = spawner.room.energyCapacityAvailable
+    let selectParts= parts.filter(p=>{
+      max-=PART_COST[p]
+      return max >=0
+    })
 
-	spawnAsap(spawner, parts, memory) {
-		if (!spawner.spawning) {
-			var result = spawner.createCreep(parts, null, memory);
-			if (result === ERR_NOT_ENOUGH_ENERGY) {
-				spawner.createCreep(parts.slice(0, 3), null, memory);
-			}
-		}
-	}
+    if (!spawner.spawning && spawner.canCreateCreep(selectParts) === 0) {
+      spawner.createCreep(selectParts, null, memory)
+    }
+  }
 
-	getCreepInNeed(){
-		return this.en_creeps.splice(0,1)
-	}
+  spawnAsap (spawner, parts, memory) {
+    if (!spawner.spawning) {
+      var result = spawner.createCreep(parts, null, memory);
+      if (result === ERR_NOT_ENOUGH_ENERGY) {
+        spawner.createCreep(parts.slice(0, 3), null, memory);
+      }
+    }
+  }
+
+  getCreepInNeed () {
+    return this.en_creeps.splice(0, 1)
+  }
 }
 
 module.exports = new CreepManager();

4e904aa889a71ac580856b0a59c04b6e8f3f7f51
St-Ex <stexthomas@gmail.com>
2016-07-04 23:26:29 +0200

    fix get energy

--- controller.js
+++ controller.js
@@ -72,8 +72,8 @@ module.exports = class Controller {
         filter: (d=>d.amount >= TRIGGER_PICK)
       })
       if (sources.length) {
-        if (creep.pickup(targets[ 0 ]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[ 0 ]);
+        if (creep.pickup(sources[ 0 ]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(sources[ 0 ]);
         }
       }
       else {

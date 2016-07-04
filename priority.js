8b5307df51fdbba8965e43bdad831ea463d6ddbb
Thomas de Saint-Exupéry <thomas.desaintexupery@bappli.com>
2016-07-04 12:23:55 +0200

    FIX PRIORITIES

--- priority.js
+++ priority.js
@@ -1,20 +1,18 @@
+const PRIORITIES = {
+	STRUCTURE_SPAWN:       10,
+	STRUCTURE_EXTENSION:   8,
+	STRUCTURE_TOWER:       7,
+	STRUCTURE_ROAD:        -2,
+	STRUCTURE_WALL:        1,
+	STRUCTURE_RAMPART:     2,
+	STRUCTURE_CONTROLLER:  9,
+	STRUCTURE_STORAGE:     5,
+	STRUCTURE_CONTAINER:   6
+}
+
 module.exports = {
-	priorities() {
-		return {
-			STRUCTURE_SPAWN:       10,
-			STRUCTURE_EXTENSION:   8,
-			STRUCTURE_TOWER:       7,
-			STRUCTURE_ROAD:        -2,
-			STRUCTURE_WALL:        1,
-			STRUCTURE_RAMPART:     2,
-			STRUCTURE_CONTROLLER:  9,
-			STRUCTURE_STORAGE:     5,
-			STRUCTURE_CONTAINER:   6
-		}
-	},
-	
 	getPriority(type){
-		let weight = priorities()[type]
+		let weight = PRIORITIES[type]
 		return (weight?weight:0)
 	}
 }

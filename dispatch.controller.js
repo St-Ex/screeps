5216ed3b031de8e0bc308cf1658ad716638884e1
St-Ex <stexthomas@gmail.com>
2016-07-05 08:25:29 +0200

    better dispatcher focus

--- dispatch.controller.js
+++ dispatch.controller.js
@@ -3,38 +3,43 @@ const CreepManager = require('creep.manager')
 
 class DispatchController extends Controller {
 
-	constructor() {
-		super('dispatch')
-	}
+  constructor () {
+    super('dispatch')
+  }
 
-	control() {
-		super.control();
+  control () {
+    super.control();
 
-		this.doForEachCreep(
-			creep => {
-				if (creep.carry.energy == 0) {
-					this.goGetEnergy(creep)
-				}
-				else {
-					if (!creep.memory.target) {
-						creep.memory.target = CreepManager.getCreepInNeed().id
-					}
+    this.doForEachCreep(
+      creep => {
+        if (creep.carry.energy == 0) {
+          this.goGetEnergy(creep)
+        }
+        else {
+          if (!creep.memory.target) {
+            creep.memory.target = CreepManager.getCreepInNeed()
+          }
 
-					let target = Game.getObjectById(creep.memory.target)
+          let target = Game.getObjectById(creep.memory.target)
 
-					if (target) {
-						if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-							creep.moveTo(target);
-						}
-					}
-				}
-			}
-		)
-	}
+          if (target) {
+            switch (creep.transfer(target, RESOURCE_ENERGY)) {
+              case  ERR_NOT_IN_RANGE :
+                creep.moveTo(target)
+                break
+              case 0 :
+              default:
+                delete creep.memory.target
+            }
+          }
+        }
+      }
+    )
+  }
 
-	newCreep() {
-		return [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY]
-	}
+  newCreep () {
+    return [ MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY ]
+  }
 }
 
 module.exports = new DispatchController()

4f9eb552a26629795f13d2d779853f79ca374517
Thomas de Saint-Exupéry <thomas.desaintexupery@bappli.com>
2016-07-06 12:53:14 +0200

    Repair road

--- build.controller.js
+++ build.controller.js
@@ -3,76 +3,85 @@ const PRIOS = require('priority.manager')
 
 class BuildController extends Controller {
 
-  constructor () {
-    super('build', true)
-  }
+	constructor() {
+		super('build', true)
+	}
 
-  control () {
-    super.control();
+	control() {
+		super.control();
 
-    this.doForEachCreep(
-      creep => {
-        creep.memory.build = (
-          creep.memory.build && creep.carry.energy !== 0
-          || !creep.memory.build && creep.carry.energy === creep.carryCapacity
-        )
+		this.doForEachCreep(
+			creep => {
+				creep.memory.build = (
+					creep.memory.build && creep.carry.energy !== 0
+					|| !creep.memory.build && creep.carry.energy === creep.carryCapacity
+				)
 
-        if (!creep.memory.build) {
-          this.goGetEnergy(creep)
-        }
-        else {
-          let target
-          if (creep.memory.target) {
-            target = Game.getObjectById(creep.memory.target)
+				if (!creep.memory.build) {
+					this.goGetEnergy(creep)
+				}
+				else {
+					let target
+					if (creep.memory.target) {
+						target = Game.getObjectById(creep.memory.target)
 
-            if (!target || target.hits === target.hitsMax) {
-              target = false;
-            }
-          }
+						if (!target || target.hits === target.hitsMax) {
+							target = false;
+						}
+					}
 
-          if (!target) {
-            let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            creep.memory.repair = false
-            if (!targets.length) {
-              targets = creep.room.find(
-                FIND_MY_STRUCTURES, { filter: structure => structure.hits < structure.hitsMax }
-              )
-              creep.memory.repair = true
-            }
+					if (!target) {
+						let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+						creep.memory.repair = false
+						if (!targets.length) {
+							targets = creep.room.find(
+								FIND_MY_STRUCTURES, {filter: s => s.hits < s.hitsMax}
+							)
+							if (!targets.length) {
+								targets = creep.room.find(
+									FIND_STRUCTURES, {
+										filter: s => s.structureType === STRUCTURE_ROAD
+										&& s.hits < s.hitsMax
+									}
+								)
+							}
+							creep.memory.repair = true
+						}
 
-            targets.sort(
-              (t1, t2) => PRIOS.getPriority(t2.structureType) - PRIOS.getPriority(
-                t1.structureType
-              )
-            )
+						targets.sort(
+							(t1, t2) => PRIOS.getPriority(t2.structureType) - PRIOS.getPriority(
+								t1.structureType
+							)
+						)
 
-            target = targets[ 0 ]
-          }
+						target = targets[0]
+					}
 
-          creep.memory.target = target.id
+					creep.memory.target = target.id
 
-          let result
-          // Build or repare target
-          if (creep.memory.repair) {
-            result = creep.repair(target)
-          }
-          else {
-            result = creep.build(target)
-          }
+					let result
+					// Build or repare target
+					if (creep.memory.repair) {
+						result = creep.repair(target)
+					}
+					else {
+						result = creep.build(target)
+					}
 
-          if (result === ERR_NOT_IN_RANGE) {
-            creep.moveTo(target);
-          } else if (result == ERR_INVALID_TARGET) {
-            delete creep.memory.target
-          }
-        }
-      }
-    )
-  }
+					if (result === ERR_NOT_IN_RANGE) {
+						creep.moveTo(target);
+					}
+					else if (result == ERR_INVALID_TARGET) {
+						delete creep.memory.target
+					}
+				}
+			}
+		)
+	}
 
-  newCreep () {
-    return [ WORK, CARRY, MOVE, WORK, CARRY, CARRY, WORK ]
-  }
+	newCreep() {
+		return [WORK, CARRY, MOVE, WORK, CARRY, CARRY, WORK]
+	}
 
 }
 

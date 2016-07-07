05e23bcf379e2d39ff1a53cf734e0dae8dcf96d7
St-Ex <stexthomas@gmail.com>
2016-07-07 07:52:03 +0200

    Do not harvest if full

--- harvest.controller.js
+++ harvest.controller.js
@@ -2,34 +2,36 @@ const Controller = require('controller')
 
 class HarvestController extends Controller {
 
-	constructor() {
-		super('harvest')
-	}
+  constructor () {
+    super('harvest')
+  }
 
-	control() {
-		super.control();
-		this.doForEachCreep(this.harvestOrMove.bind(this))
-	}
+  control () {
+    super.control();
+    this.doForEachCreep(this.harvestOrMove.bind(this))
+  }
 
-	harvestOrMove(creep) {
-		if (!creep.memory.target) {
-			let sources = creep.room.find(FIND_SOURCES);
-			let result = creep.harvest(sources[0])
-			if (result === 0) {
-				creep.memory.target = sources[0].id
-			}
-			else if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(sources[0]);
-			}
-		}
-		else {
-			creep.harvest(Game.getObjectById(creep.memory.target))
-		}
-	}
+  harvestOrMove (creep) {
+    if (!creep.memory.target) {
+      let sources = creep.room.find(FIND_SOURCES);
+      let result = creep.harvest(sources[ 0 ])
+      if (result === 0) {
+        creep.memory.target = sources[ 0 ].id
+      }
+      else if (creep.harvest(sources[ 0 ]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[ 0 ]);
+      }
+    }
+    else {
+      if (creep.carry.energy !== creep.carryCapacity) {
+        creep.harvest(Game.getObjectById(creep.memory.target))
+      }
+    }
+  }
 
-	newCreep() {
-		return [MOVE, WORK, CARRY, WORK, CARRY, CARRY, CARRY, CARRY]
-	}
+  newCreep () {
+    return [ MOVE, WORK, CARRY, WORK, CARRY, CARRY, CARRY, CARRY ]
+  }
 }
 
 module.exports = new HarvestController();

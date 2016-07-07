20d258d1bb78091ec110168bf7da7c4cd4401d1e
Thomas de Saint-Exupéry <thomas.desaintexupery@bappli.com>
2016-07-07 12:03:52 +0200

    Source affectation

--- source.manager.js
+++ source.manager.js
@@ -1,43 +1,51 @@
 class SourceManager {
 
-  reset () {
-    this.sources = {
-      E32S39: {
-        '576a9c8e57110ab231d8947b': { max: 2, creeps: 0 },
-        '576a9c8e57110ab231d8947c': { max: 2, creeps: 0 }
-      }
-  }
-  }
+	reset() {
+		this.sources = {
+			E32S39: {
+				'576a9c8e57110ab231d8947b': {max: 2, creeps: 0},
+				'576a9c8e57110ab231d8947c': {max: 2, creeps: 0}
+			}
+		}
+	}
 
-  addCreep (creep) {
-    if (this.sources[ creep.room.name ]
-      && this.sources[ creep.room.name ][ creep.memory.target ]) {
-      this.sources[ creep.room.name ][ creep.memory.target ].creeps++
-    }
-  }
+	addCreep(creep) {
+		if (this.sources[creep.room.name]
+			&& this.sources[creep.room.name][creep.memory.target]) {
+			let src = this.sources[creep.room.name][creep.memory.target];
 
-  findAvailableSource (creep) {
-    if (!this.sources[ creep.room.name ]) {
-      let sources = creep.room.find(FIND_SOURCES)
-      creep.memory.target = sources[ 0 ].id
-    } else {
-      for (sourceId in SOURCES[ creep.room.name ]) {
-        let s = SOURCES[ creep.room.name ][ sourceId ]
-        if (s.creeps < s.max) {
-          creep.memory.target = sourceId
-          this.addCreep(creep)
-          break
-        }
-      }
-    }
-    console.log('Affect',creep.name,creep.memory.target)
-  }
+			if (src.creeps < src.max) {
+				this.sources[creep.room.name][creep.memory.target].creeps++
+			}
+			else {
+				delete creep.memory.target
+			}
+		}
+	}
 
-  getMax (roomId, sourceId) {
-    if (!SOURCES[ roomId ]) return 0
-    if (!SOURCES[ roomId ][ sourceId ]) return 0
-    return SOURCES[ roomId ][ sourceId ].max
-  }
+	findAvailableSource(creep) {
+		if (!this.sources[creep.room.name]) {
+			let sources = creep.room.find(FIND_SOURCES)
+			creep.memory.target = sources[0].id
+		}
+		else {
+			for (sourceId in SOURCES[creep.room.name]) {
+				let s = SOURCES[creep.room.name][sourceId]
+				if (s.creeps < s.max) {
+					creep.memory.target = sourceId
+					this.addCreep(creep)
+					break
+				}
+			}
+		}
+		console.log('Affect', creep.name, creep.memory.target)
+	}
+
+	getMax(roomId, sourceId) {
+		if (!SOURCES[roomId]) return 0
+		if (!SOURCES[roomId][sourceId]) return 0
+		return SOURCES[roomId][sourceId].max
+	}
 }
 
 module.exports = new SourceManager()
